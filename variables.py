8478152fa55f6256985bf41f30bf9ed260e810f4
Kyshino <delaplazaalberto@gmail.com>
2024-10-30 23:38:58 +0100

    Disable A5 format

--- variables.py
+++ variables.py
@@ -12,7 +12,7 @@ sizes = {
 paper_horizontal_sizes = {
     "A4": (3508, 2480),  # 210 mm x 297 mm (horizontal)
     "A3": (4961, 3508),  # 297 mm x 420 mm (horizontal)
-    "A5": (2480, 1748),  # 148 mm x 210 mm (horizontal)
+    # "A5": (2480, 1748),  # 148 mm x 210 mm (horizontal)
     "Carta": (3508, 2551),  # 216 mm x 279 mm (horizontal)
     "Legal": (4211, 2551),  # 216 mm x 356 mm (horizontal)
     "B5": (2953, 2079)   # 176 mm x 250 mm (horizontal)
